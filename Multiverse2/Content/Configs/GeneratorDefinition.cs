7fcd4236728a3fda0642d8620f4291eff74c6317
potatiumstotalis <83508111+potatiumstotalis@users.noreply.github.com>
2023-12-13 13:35:15 +0000

    1st Upload

---
+++ Multiverse2/Content/Configs/GeneratorDefinition.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ComponentModel;
+using Multiverse2.Content.Configs.UI;
+using Multiverse2.Content.Generators;
+using Terraria.ModLoader;
+using Terraria.ModLoader.Config;
+using Terraria.ModLoader.IO;
+
+namespace Multiverse2.Content.Configs
+{
+	[TypeConverter(typeof(ToFromStringConverter<GeneratorDefinition>))]
+	[CustomModConfigItem(typeof(GeneratorDefinitionElement))]
+	public class GeneratorDefinition : EntityDefinition
+	{
+		public static readonly Func<TagCompound, GeneratorDefinition> DESERIALIZER = Load;
+
+		public GeneratorDefinition()
+			: this(-1)
+		{
+		}
+
+		public GeneratorDefinition(int type)
+			: base(type >= 0? GeneratorLoader.Get(type).FullName : "Terraria/None")
+		{
+		}
+
+		public GeneratorDefinition(string key)
+			: base(key)
+		{
+		}
+
+		public GeneratorDefinition(string mod, string name)
+			: base(mod, name)
+		{
+		}
+
+		public override int Type =>
+			!ModContent.TryFind<ModGenerator>(Mod != "Terraria" ? Mod + "/" + Name : Name, out var gen) ? -1 : gen.Type;
+
+		public static GeneratorDefinition FromString(string s)
+		{
+			return new GeneratorDefinition(s);
+		}
+
+		public static GeneratorDefinition Load(TagCompound tag)
+		{
+			return new GeneratorDefinition(tag.GetString("mod"), tag.GetString("name"));
+		}
+	}
+}
---
+++ Multiverse2/Content/Subworlds/MultiverseWorld.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Multiverse2.Content.Configs;
+using Multiverse2.Content.Generators;
+using SubworldLibrary;
+using Terraria;
+using Terraria.GameContent.UI.States;
+using Terraria.ModLoader;
+using Terraria.WorldBuilding;
+
+namespace Multiverse2.Content.Subworlds
+{
+	public class MultiverseWorld : Subworld
+	{
+		private UIWorldLoad _menu;
+
+		public MultiverseWorld(MultiverseWorldConfiguration gen)
+		{
+			Gen = gen;
+		}
+
+		private MultiverseWorldConfiguration Gen { get; }
+
+		public override string Name => Gen.Name.Replace(" ", "");
+
+		public override bool ShouldSave => Gen.Saving;
+
+		public override bool NormalUpdates => true;
+
+		public override int Width => Gen.Width;
+
+		public override int Height => Gen.Height;
+
+		public override WorldGenConfiguration Config => WorldGenConfiguration.FromEmbeddedPath("Terraria.GameContent.WorldBuilding.Configuration.json");
+
+		public override List<GenPass> Tasks => GeneratorLoader.Get(Gen.Generator.Type).GetPasses(Gen, Config);
+
+
+		public override void DrawMenu(GameTime gameTime)
+		{
+			if (WorldGenerator.CurrentGenerationProgress != null)
+				(_menu ??= new UIWorldLoad()).Draw(Main.spriteBatch);
+			else
+				base.DrawMenu(gameTime);
+		}
+	}
+}
