fd9df2854af1d9be71ae19f44863296c0d4a3beb
potatiumstotalis <83508111+potatiumstotalis@users.noreply.github.com>
2023-12-15 16:38:41 +0000

    Main 0.1.0 Update

---
+++ MultiverseRel/Content/Configs/UI/GeneratorDefinitionOptionElement.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Xna.Framework.Graphics;
+using MultiverseRel.Content.Generators;
+using Terraria.Localization;
+using Terraria.ModLoader.UI;
+
+namespace MultiverseRel.Content.Configs.UI
+{
+	public class GeneratorDefinitionOptionElement : DefinitionOptionElement<GeneratorDefinition>
+	{
+		private readonly UIAutoScaleTextTextPanel<string> _text;
+
+		public GeneratorDefinitionOptionElement(GeneratorDefinition definition, float scale = 0.75f)
+			: base(definition, scale)
+		{
+			Width.Set(150f * scale, 0.0f);
+			Height.Set(40f * scale, 0.0f);
+			var scaleTextTextPanel =
+				new UIAutoScaleTextTextPanel<string>(type == -1 ? "None" : Language.GetTextValue(
+					$"{GeneratorLoader.Get(type).Name}"));
+			scaleTextTextPanel.Width.Percent = 1f;
+			scaleTextTextPanel.Height.Percent = 1f;
+			_text = scaleTextTextPanel;
+			Append(_text);
+		}
+
+        public override void SetItem(GeneratorDefinition item)
+        {
+            base.SetItem(item);
+            _text?.SetText(type == -1 ? "None" : Language.GetTextValue(
+                $"Mods.{GeneratorLoader.Get(type).Mod.Name}.GeneratorName.{GeneratorLoader.Get(type).Name}"));
+        }
+
+
+        public override void SetScale(float scale)
+		{
+			base.SetScale(scale);
+			Width.Set(150f * scale, 0.0f);
+			Height.Set(40f * scale, 0.0f);
+		}
+
+		protected override void DrawSelf(SpriteBatch spriteBatch)
+		{
+			if (!IsMouseHovering)
+				return;
+			var fieldInfo = Type.GetType("Terraria.ModLoader.Config.UI.UIModConfig")
+				?.GetField("tooltip", BindingFlags.Static);
+			fieldInfo?.SetValue(null, tooltip);
+		}
+	}
+}
---
+++ MultiverseRel/Content/Generators/ModGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using MultiverseRel.Content.Configs;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.Utilities;
+using Terraria.WorldBuilding;
+
+namespace MultiverseRel.Content.Generators
+{
+	public abstract class ModGenerator : ModType
+	{
+		private static readonly UnifiedRandom Rand = new();
+
+		public int Type { get; private set; }
+
+		public virtual string DisplayName => Name.Replace("Generator", "");
+
+		protected virtual int Seed => Rand.Next();
+
+		public List<GenPass> GetPasses(MultiverseWorldConfiguration configuration, WorldGenConfiguration genConfiguration)
+		{
+			return GenPasses(SetupPasses(configuration), genConfiguration);
+		}
+
+		protected abstract List<GenPass> GenPasses(int seed, WorldGenConfiguration configuration);
+
+		private int SetupPasses(MultiverseWorldConfiguration configuration)
+		{
+			var seed = configuration.Seed == 0 ? Seed : configuration.Seed;
+			WorldGen._lastSeed = seed;
+			WorldGen._genRand = new UnifiedRandom(seed);
+			Main.rand = new UnifiedRandom(seed);
+			return seed;
+		}
+
+		protected sealed override void Register()
+		{
+			Type = GeneratorLoader.Add(this);
+		}
+
+		public sealed override void SetupContent()
+		{
+			SetStaticDefaults();
+		}
+	}
+}
